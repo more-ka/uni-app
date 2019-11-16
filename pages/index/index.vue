a046eb8c9bc12847b45399fc680c7772cae2e062
more-ka <1640627628@qq.com>
2019-11-16 23:04:22 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">首页</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
