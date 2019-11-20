fa03f81a01f388570d17c98f902c62545bbc0c57
more-ka <1640627628@qq.com>
2019-11-21 00:14:27 +0800

    用户/登录页面

---
+++ pages/login/login.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="page" :style="{height: pageHeight + 'px'}">
+		<view class="picture">
+      <image src="../../static/icos/default-face.png" mode=""></image>
+    </view>
+    <view class="username">
+      <view class="text">账号</view>
+      <input type="text" placeholder="请输入用户名">
+    </view>
+    <view class="password">
+      <view class="text">密码</view>
+      <input type="text" password="true" placeholder="请输入密码">
+    </view>
+    <button type="primary" class="login">注册/登录</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pageHeight: 0
+			}
+		},
+    onLoad(){
+      let pageHeight = uni.getSystemInfoSync().windowHeight
+      this.pageHeight = pageHeight
+    },
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+  @import url("login.css");
+</style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,22 +1,37 @@
 <template>
-	<view>
-		用户页面
-	</view>
+    <view class="page" :style="{height: height + 'px'}">
+      <view class="header">
+        <view class="picture">
+          <image src="../../static/icos/default-face.png" mode=""></image>
+        </view>
+        <button @click="login">注册/登录</button>
+      </view>
+    </view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
+        height: 100
 			}
 		},
+    onLoad() {
+      let height = uni.getSystemInfoSync().windowHeight
+      this.height = height
+    },
 		methods: {
-			
+			login(){
+        uni.navigateTo({
+          url: '/pages/login/login'
+        })
+      }
 		}
 	}
 </script>
 
 <style>
-
+ @import url("user.css");
+ .page{
+ }
 </style>
